409a8c1a30e0a83c35d79ca0e7af409e570a42ad
ignacio poletti <ip@discoverit.store>
2024-12-15 15:21:09 -0300

    FEATURE | Added carousel to car card

--- app/components/car-card/index.tsx
+++ app/components/car-card/index.tsx
@@ -6,11 +6,22 @@ import { NumberFormatter } from "@/app/utils";
 import { Button, Chip } from "@nextui-org/react";
 import { FC, useEffect, useState } from "react";
 import { BsHeart, BsHeartFill } from "react-icons/bs";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 interface CarCardProps {
   car: ICarType;
   gridMode: boolean;
 }
+
+const carImages: string[] = [
+  'https://s3-alpha-sig.figma.com/img/210d/0755/dd24cb0bde29a2119e23f3236f743cb1?Expires=1734912000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=OZzB022UCqsUrhwyzFZyKZszyEylpxPoOtkj-lCQoZvGkhO787Ko0ANqixcqkTSANDEPzMuUMHez2Rl9t2M2goJTqXPjZgKgyhHBlICuMLAvsLVrVfM28CqgK7jy5maJuct-QaYIe7gR5r-rXX1qZFKqXKcZMu1kCzsdvPoLnPh8N-DoT3PKLlVx4QlrEDargzSS1bh-jmn9WHGy7i2KDemgalKn9sw53B2pr9mjiTmI6Bm8~I7JWG7V-aQikgbT5mm2GnvnOWcZmOK4eUgM3R4NsRZcb0htyayUbilA~fJ0nNzfWM2CzJDYeHTc2M0Ou0gJ0qV36oXsbODim2QlCw__', 
+  'https://s3-alpha-sig.figma.com/img/2759/ca26/75dc4ba63c6906f49bbaa32f101780ae?Expires=1734912000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=SEj5-ixy16k7JHNcUCwuMm7wkEUVxoXrSBlIubUOG3N2HqjcYk2eqsB9BCLTbqRDiUfIoIYpvyw-D9KiAM9IQtqRlNR7zkLm61XMAatlQ7SpLtkijvfQuE5PNpfUi3y7iXrf9j5rF4K7uZ5ngjMaBZkfXNSdPEh1z5FEYEmT~jvPKa1zdif0kiTtCVJZ3~cpAoF5KEau527ZJBc-CAuV17Tcct39GLZ6ej-ztRDlF9i18QhPHhKm6~184YRAyhJ3Uv7FGH5vJkFV~Mt2TC9qo07AIrni4P1PXaE97mq6lszzIbW9QP36ejfiTXJQL6mKJ8trGHK1YOTmQO2qN5XjQQ__',
+  'https://motormagazine.com.ar/wp-content/uploads/2019/03/fiat-uno-way-1-1.jpg'
+];
 
 export const CarCard: FC<CarCardProps> = ({ car, gridMode }) => {
   const [isFavorite, setIsFavorite] = useState<boolean>(false);
@@ -35,7 +46,6 @@ export const CarCard: FC<CarCardProps> = ({ car, gridMode }) => {
       setIsFavorite(true);
     }
   };
-    
 
   return (
     <div
@@ -62,12 +72,31 @@ export const CarCard: FC<CarCardProps> = ({ car, gridMode }) => {
           }`}
           onPress={toggleFavorite}
         >
-          {!isFavorite ? <BsHeart /> : <BsHeartFill className="text-blue-700 "/>}
+          {!isFavorite ? <BsHeart /> : <BsHeartFill className="text-blue-700 " />}
         </Button>
-        <img
-          className="h-full w-full object-cover"
-          src="https://s3-alpha-sig.figma.com/img/210d/0755/dd24cb0bde29a2119e23f3236f743cb1?Expires=1734912000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=OZzB022UCqsUrhwyzFZyKZszyEylpxPoOtkj-lCQoZvGkhO787Ko0ANqixcqkTSANDEPzMuUMHez2Rl9t2M2goJTqXPjZgKgyhHBlICuMLAvsLVrVfM28CqgK7jy5maJuct-QaYIe7gR5r-rXX1qZFKqXKcZMu1kCzsdvPoLnPh8N-DoT3PKLlVx4QlrEDargzSS1bh-jmn9WHGy7i2KDemgalKn9sw53B2pr9mjiTmI6Bm8~I7JWG7V-aQikgbT5mm2GnvnOWcZmOK4eUgM3R4NsRZcb0htyayUbilA~fJ0nNzfWM2CzJDYeHTc2M0Ou0gJ0qV36oXsbODim2QlCw__"
-        />
+        <Swiper
+          spaceBetween={10}
+          slidesPerView={1}
+          loop
+          pagination={{ clickable: true }}
+          modules={[Pagination]}
+          className="w-full h-full"
+          style={{
+            '--swiper-pagination-color' : '#FFFFFF',
+            '--swiper-pagination-bullet-inactive-color': '#FFFFFF',
+            '--swiper-pagination-bullet-inactive-opacity': '50%'
+          }}
+        >
+          {carImages.map((imgSrc, index) => (
+            <SwiperSlide key={index}>
+              <img
+                className="h-full w-full object-cover"
+                src={imgSrc}
+                alt={`Car image ${index + 1}`}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
       <div className="flex flex-col gap-1">
         <div className="flex gap-2 items-center">
